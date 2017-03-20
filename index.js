4daaea6037b31b4f7fbc9dda608ca59ee47cd6fd
Leei <raysix900202@gmail.com>
2017-03-20 22:16:10 +0800

    update

--- index.js
+++ index.js
@@ -3,18 +3,19 @@
 const template = require('art-template')
 
 const viewsEngine = (path, {
+  extname = '.html',
+  encodeing = 'utf-8'
 } = {}) => {
   return (ctx, next) => {
-    if (ctx.render) {
-      return next()
-    }
+    if (!ctx.render) {
+      template.config('base', path)
+      template.config('extname', extname)
+      template.config('encodeing', encodeing)
 
-    ctx.render = (tmpPath, datas = {}) => {
-      let realPath = path + '/' + tmpPath
-      let html = template(realPath, datas)
-      ctx.body = html
+      ctx.render = (tmpPath, datas = {}) => {
+        ctx.body = template(tmpPath, datas)
+      }
     }
-
     return next()
   }
 }
